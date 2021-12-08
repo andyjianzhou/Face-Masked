40152817febd1e50a5f429a924f6a8f3e40664d6
PandaOnTheFly <74941296+andyjianzhou@users.noreply.github.com>
2021-12-08 11:40:51 -0700

    Working webcam

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -1,5 +1,31 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
+import cv2
+# from streamlit_webrtc import webrtc_streamer #Figure out how to use this
 
-webrtc_streamer(key="example")
-st.write("Hello World")
+# webrtc_streamer(key="example")
+
+## Sidebar
+st.sidebar.title("Streamlit WebRTC")
+st.sidebar.info("This is a demo of a Streamlit WebRTC app")
+# st.sidebar.selectbox("Select a video source", ["Mask Detection", "Friend detection"])
+selection = st.sidebar.selectbox("Select a video source", ["Mask Detection", "Friend detection"])
+
+#Main webcam
+st.title(selection)
+st.title("Webcam Live Feed")
+run = st.checkbox('Run')
+FRAME_WINDOW = st.image([])
+camera = cv2.VideoCapture(0)
+while run:
+    print("Webcam is running")
+    ret, frame = camera.read()
+    if ret:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        FRAME_WINDOW.image(frame)
+    else:
+        print("Error")
+        break
+else:
+    st.write('Stopped')
+
+print("Webcam released")
