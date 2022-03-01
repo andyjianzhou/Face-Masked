f1dae3e29349eee076ba76c39e9f087134f7ce95
PandaOnTheFly <74941296+andyjianzhou@users.noreply.github.com>
2022-02-28 18:20:45 -0700

    Updated requirements

--- Streamlit/apps/UploadDet.py
+++ Streamlit/apps/UploadDet.py
@@ -1,8 +1,6 @@
 import streamlit as st
 from PIL import Image
 #Opencv and torch utils
-import cv2
-import torch
 from faster_utils import FaceMaskDataset, get_predictions, get_model_instance_segmentation, torch_to_pil, plot_img_bbox, apply_nms
 from win10toast import ToastNotifier
 toast = ToastNotifier()
